0c45caa3aa7317c1be4e49affc682fcb3585af7c
KaBrz23 <kauefernandesbraz@gmail.com>
2024-05-20 20:39:18 -0300

    atualizar main

--- src/main/java/com/example/sorteio/SorteioApplication.java
+++ src/main/java/com/example/sorteio/SorteioApplication.java
@@ -2,6 +2,8 @@ package com.example.sorteio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -20,6 +22,12 @@ public class SorteioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SorteioApplication.class, args);
+	}
+
+	@RequestMapping
+	@ResponseBody
+	public String home(){
+		return "Sorteio";
 	}
 
 }
