862ccf146830e93f1eae326b7ef0dff7b30c94ac
KaBrz23 <kauefernandesbraz@gmail.com>
2024-05-20 20:26:59 -0300

    atualizando swagger

--- src/main/java/com/example/sorteio/SorteioApplication.java
+++ src/main/java/com/example/sorteio/SorteioApplication.java
@@ -3,7 +3,19 @@ package com.example.sorteio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "API de sorteio de números",
+		description = "Sorteia números de 1 a 6 e também entre 1 e o número definido",
+		contact = @Contact(name = "Kauê Braz", email = "rm97768@fiap.com.br"),
+		version = "1.0.0"
+	)
+)
 public class SorteioApplication {
 
 	public static void main(String[] args) {
