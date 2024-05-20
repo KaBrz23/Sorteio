4b586208bcb045060a12393dd0b8c9b56c18f06b
KaBrz23 <kauefernandesbraz@gmail.com>
2024-05-20 20:24:06 -0300

    Atualizando model

--- src/main/java/com/example/sorteio/repository/SorteioRepository.java
+++ src/main/java/com/example/sorteio/repository/SorteioRepository.java
@@ -1,5 +1,10 @@
 package com.example.sorteio.repository;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class SorteioRepository {
-    
+    private int dado;
 }
